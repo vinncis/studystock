09ca4c84aaf0b74f5e600bd94d72d71e2a5b6897
vinncis <vinncis@gmail.com>
2021-04-02 01:15:42 +0900

    초기 커밋

---
+++ batch/src/main/java/com/studystock/mystock/MystockApplication.java
@@ -0,0 +1,13 @@
+package com.studystock.mystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystockApplication.class, args);
+	}
+
+}
---
+++ batch/src/test/java/com/studystock/mystock/MystockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studystock.mystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
